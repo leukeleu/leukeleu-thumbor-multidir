59e3a145929b348138bf4736e2485e2309e8fee6
Jaap Roes <jroes@leukeleu.nl>
2023-03-14 17:17:29 +0100

    Update tests to actually use multiple directories
    Remove some tests that cover base file loader (and are included with Thumbor)

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -9,36 +9,42 @@ from tornado.testing import gen_test
 
 from tc_multidir.loader import load
 
-STORAGE_PATH = abspath(join(dirname(__file__), "../fixtures/images/"))
+PRIMARY_IMAGES_PATH = abspath(join(dirname(__file__), "../fixtures/images/"))
+SECONDARY_IMAGES_PATH = abspath(join(dirname(__file__), "../fixtures/more-images/"))
 
 
 class FileLoaderTestCase(TestCase):
     def setUp(self):
         super().setUp()
-        config = Config(TC_MULTIDIR_PATHS=[STORAGE_PATH])
+        config = Config(
+            TC_MULTIDIR_PATHS=[
+                PRIMARY_IMAGES_PATH,
+                SECONDARY_IMAGES_PATH,
+            ]
+        )
         self.ctx = Context(config=config)
 
     async def load_file(self, file_name):
         return await load(self.ctx, file_name)
 
     @gen_test
-    async def test_should_load_file(self):
+    async def test_should_load_file_in_primary_dir(self):
         result = await self.load_file("screenshot.png")
         expect(result).to_be_instance_of(LoaderResult)
-        with open(join(STORAGE_PATH, "screenshot.png"), "rb") as img:
+        with open(join(PRIMARY_IMAGES_PATH, "screenshot.png"), "rb") as img:
             expect(result.buffer).to_equal(img.read())
         expect(result.successful).to_be_true()
 
     @gen_test
-    async def test_should_load_file_with_extended_utf8(self):
-        result = await self.load_file("utf8 æèëðġŒꭦ.png")
+    async def test_should_load_file_in_secondary_dir(self):
+        result = await self.load_file("image with spaces.png")
         expect(result).to_be_instance_of(LoaderResult)
-        with open(join(STORAGE_PATH, "utf8 æèëðġŒꭦ.png"), "rb") as img:
+        with open(join(SECONDARY_IMAGES_PATH, "image with spaces.png"), "rb") as img:
             expect(result.buffer).to_equal(img.read())
         expect(result.successful).to_be_true()
 
     @gen_test
-    async def test_should_fail_when_inexistent_file(self):
+    async def test_should_fail_when_non_existent_file(self):
         result = await self.load_file("image_NOT.jpg")
         expect(result).to_be_instance_of(LoaderResult)
         expect(result.buffer).to_equal(None)
@@ -50,19 +56,3 @@ class FileLoaderTestCase(TestCase):
         expect(result).to_be_instance_of(LoaderResult)
         expect(result.buffer).to_equal(None)
         expect(result.successful).to_be_false()
-
-    @gen_test
-    async def test_should_load_file_with_spaces_in_name(self):
-        result = await self.load_file("image with spaces.png")
-        expect(result).to_be_instance_of(LoaderResult)
-        with open(join(STORAGE_PATH, "image with spaces.png"), "rb") as img:
-            expect(result.buffer).to_equal(img.read())
-        expect(result.successful).to_be_true()
-
-    @gen_test
-    async def test_should_load_file_with_spaces_encoded_in_name(self):
-        result = await self.load_file("image%20with%20spaces.png")
-        expect(result).to_be_instance_of(LoaderResult)
-        with open(join(STORAGE_PATH, "image%20with%20spaces.png"), "rb") as img:
-            expect(result.buffer).to_equal(img.read())
-        expect(result.successful).to_be_true()
