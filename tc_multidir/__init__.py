fcf0aa7ec5819fb2579e2867f47f1243638ca440
Jaap Roes <jroes@leukeleu.nl>
2023-03-14 16:40:15 +0100

    Remove coding: utf-8 comments

--- tc_multidir/__init__.py
+++ tc_multidir/__init__.py
@@ -1,4 +1,3 @@
- # -*- coding: utf-8 -*-
 import os
 
 from ._version import __version__
--- tc_multidir/loader.py
+++ tc_multidir/loader.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 from thumbor.config import Config
 from thumbor.context import Context
 from thumbor.loaders import LoaderResult, file_loader
