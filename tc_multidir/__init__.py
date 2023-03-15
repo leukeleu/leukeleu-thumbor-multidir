3a61e981a253f7d9214139c2d931b9944c4dd6c9
Jaap Roes <jroes@leukeleu.nl>
2023-03-15 11:21:45 +0100

    Remove _version module

--- tc_multidir/__init__.py
+++ tc_multidir/__init__.py
@@ -1,10 +1,8 @@
 from thumbor.config import Config
 
-from ._version import __version__
-
 Config.define(
     "TC_MULTIDIR_PATHS",
     [],
-    "The list of paths where the File Loader will try to find images",
+    "The list of paths where the multidir loader will try to find images",
     "File Loader",
 )
--- tc_multidir/_version.py
+++
@@ -1,5 +0,0 @@
-"""
-This module defines the package version for use in __init__.py and
-setup.py.
-"""
-__version__ = "0.2.8"
